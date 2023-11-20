06593e06438df1b022a0386689b429bda1bdce9e
Matheus França <99504777+Matheus-Franca-x@users.noreply.github.com>
2023-11-20 13:23:31 -0300

    Add files via upload

---
+++ fornecedor_pedido-exerc5.sql
@@ -0,0 +1,134 @@
+
+USE master
+
+DROP DATABASE fornecedor_pedido
+
+CREATE DATABASE fornecedor_pedido
+GO
+USE fornecedor_pedido
+GO
+CREATE TABLE fornecedor
+(
+	codigo INT IDENTITY(1001, 1) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	atividade VARCHAR(30) NOT NULL,
+	telefone CHAR(13) NOT NULL
+	PRIMARY KEY (codigo)
+)
+GO
+CREATE TABLE produto
+(
+	codigo INT IDENTITY(1, 1) NOT NULL,
+	nome VARCHAR(50) NOT NULL,
+	valor_unitario DECIMAL(7, 2) NOT NULL,
+	quantidade_estoque INT NOT NULL,
+	descricão VARCHAR(100) NOT NULL,
+	codigo_fornecedor INT NOT NULL
+	PRIMARY KEY (codigo)
+	FOREIGN KEY (codigo_fornecedor) REFERENCES fornecedor (codigo)
+)
+GO
+CREATE TABLE cliente
+(
+	codigo INT IDENTITY(33601, 1) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	logr_end VARCHAR(50) NOT NULL,
+	num_end INT NOT NULL,
+	telefone CHAR(13) NOT NULL,
+	data_nasc DATE NOT NULL
+	PRIMARY KEY (codigo)
+)
+GO
+CREATE TABLE pedido
+(
+	codigo INT NOT NULL,
+	codigo_cliente INT NOT NULL,
+	codigo_produto INT NOT NULL,
+	quantidade INT NOT NULL,
+	previsão_entrega DATE NOT NULL
+	PRIMARY KEY (codigo, codigo_cliente, codigo_produto)
+	FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo),
+	FOREIGN KEY (codigo_produto) REFERENCES produto (codigo)	
+)
+
+INSERT INTO fornecedor 
+VALUES 
+('Estrela',	'Brinquedo',	'41525898'),
+('Lacta',	'Chocolate',	'42698596'),
+('Asus',	'Informática',	'52014596'),
+('Tramontina',	'Utensílios Domésticos',	'50563985'),
+('Grow',	'Brinquedos',	'47896325'),
+('Mattel',	'Bonecos',	'59865898')
+
+INSERT INTO produto 
+VALUES
+('Banco Imobiliário',	65.00,	15,	'Versão Super Luxo',	1001),
+('Puzzle 5000 peças',	50.00,	5,	'Mapas Mundo',	1005),
+('Faqueiro',	350.00,	0,	'120 peças',	1004),
+('Jogo para churrasco',	75.00,	3,	'7 peças',	1004),
+('Tablet',	750.00,	29,	'Tablet',	1003),
+('Detetive',	49.00,	0,	'Nova Versão do Jogo',	1001),
+('Chocolate com Paçoquinha',	6.00,	0,	'Barra',	1002),
+('Galak',	5.00,	65,	'Barra',	1002)
+
+INSERT INTO cliente 
+VALUES
+('Maria Clara',	'R. 1° de Abril',	870,	'96325874',	'2000-08-15'),
+('Alberto Souza',	'R. XV de Novembro',	987,	'95873625',	'1985-02-02'),
+('Sonia Silva',	'R. Voluntários da Pátria',	1151,	'75418596',	'1957-08-23'),
+('José Sobrinho',	'Av. Paulista',	250,	'85236547',	'1986-12-09'),
+('Carlos Camargo',	'Av. Tiquatira',	9652,	'75896325',	'1971-03-25')
+
+INSERT INTO pedido
+VALUES
+(99001, 33601,	1,	1,	'2012-06-07'),
+(99001, 33601,	2,	1,	'2012-06-07'),
+(99001, 33601,	8,	3,	'2012-06-07'),
+(99002, 33602,	2,	1,	'2012-06-09'),
+(99002, 33602,	4,	3,	'2012-06-09'),
+(99003, 33605,	5,	1,	'2012-06-15')
+
+UPDATE produto 
+SET valor_unitario = valor_unitario * 0.9
+WHERE descricão LIKE 'Barra'
+
+UPDATE produto 
+SET quantidade_estoque = 10
+WHERE nome LIKE 'Faqueiro'
+
+SELECT c.nome, p.quantidade * pr.valor_unitario AS valor_total, (p.quantidade * pr.valor_unitario) * 0.75 AS valor_desconto
+FROM cliente c, pedido p, produto pr
+WHERE c.codigo = p.codigo_cliente 
+	AND p.codigo_produto = pr.codigo 
+	AND c.nome LIKE 'Maria%'
+	
+SELECT *
+FROM produto p, fornecedor f
+WHERE f.codigo  = p.codigo_fornecedor 
+	AND f.atividade LIKE 'Brinquedo'
+	AND p.quantidade_estoque = 0
+	
+SELECT nome, DATEDIFF(YEAR, data_nasc, GETDATE()) AS idade
+FROM cliente c 
+WHERE DATEDIFF(YEAR, data_nasc, GETDATE()) > 40 
+
+SELECT nome, telefone 
+FROM fornecedor f 
+WHERE atividade LIKE 'Chocolate' OR atividade LIKE 'Brinquedo%'
+
+SELECT nome, valor_unitario * 0.75 AS preco_desconto
+FROM produto p
+WHERE valor_unitario < 50
+
+SELECT nome, valor_unitario * 1.1 AS preco_desconto
+FROM produto p
+WHERE valor_unitario > 100
+
+SELECT p.codigo, (p.quantidade * pr.valor_unitario) * 0.85 AS preco_desconto
+FROM pedido p, produto pr
+WHERE p.codigo_produto = pr.codigo 
+	AND p.codigo = 99001
+	
+SELECT p.codigo, c.nome, DATEDIFF(YEAR, c.data_nasc, GETDATE()) AS idade 
+FROM pedido p, cliente c 
+WHERE p.codigo_cliente = c.codigo 
---
+++ fornecedor_produto-exerc4.sql
@@ -0,0 +1,121 @@
+CREATE DATABASE fornecedor_produto
+GO
+USE fornecedor_produto
+GO
+CREATE TABLE cliente
+(
+	cpf CHAR(11) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	telefone CHAR(13) NOT NULL
+	PRIMARY KEY (cpf)
+)
+GO
+CREATE TABLE fornecedor
+(
+	id INT IDENTITY(1, 1) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	logr_end VARCHAR(50) NOT NULL,
+	num_end INT NOT NULL,
+	compl_end VARCHAR(50) NOT NULL,
+	cidade_end VARCHAR(50) NOT NULL
+	PRIMARY KEY (id)
+)
+GO
+CREATE TABLE produto
+(
+	codigo INT IDENTITY(1, 1) NOT NULL,
+	descricao VARCHAR(100) NOT NULL,
+	fornecedor INT NOT NULL,
+	preco DECIMAL(7, 2) NOT NULL
+	PRIMARY KEY (codigo)
+	FOREIGN KEY (fornecedor) REFERENCES fornecedor (id)
+)
+GO
+CREATE TABLE venda
+(
+	codigo INT NOT NULL,
+	produto INT NOT NULL,
+	cliente CHAR(11) NOT NULL,
+	quantidade INT NOT NULL,
+	valor_total DECIMAL(7, 2) NOT NULL,
+	data_venda DATE NOT NULL
+	PRIMARY KEY (codigo, produto, cliente)
+	FOREIGN KEY (produto) REFERENCES produto (codigo),
+	FOREIGN KEY (cliente) REFERENCES cliente (cpf)
+)
+
+INSERT INTO cliente 
+VALUES 
+('34578909290',	'Julio Cesar',	 '82736541'),
+('25186533710',	'Maria Antonia', '87652314'),
+('87627315416',	'Luiz Carlos',	 '61289012'),
+('79182639800',	'Paulo Cesar',	 '90765273')
+
+INSERT INTO fornecedor
+VALUES
+('LG',	'Rod. Bandeirantes',	70000,	'Km 70',	'Itapeva'),
+('Asus',	'Av. Nações Unidas',	10206,	'Sala 225',	'São Paulo'),
+('AMD',	'Av. Nações Unidas',	10206,	'Sala 1095',	'São Paulo'),
+('Leadership',	'Av. Nações Unidas',	10206,	'Sala 87',	'São Paulo'),
+('Inno',	'Av. Nações Unidas',	10206,	'Sala 34',	'São Paulo')
+
+INSERT INTO produto
+VALUES
+('Monitor 19 pol.',	1,	449.99),
+('Netbook 1GB Ram 4 Gb HD',	2,	699.99),
+('Gravador de DVD - Sata',	1,	99.99),
+('Leitor de CD',	1,	49.99),
+('Processador - Phenom X3 - 2.1GHz',	3,	349.99),
+('Mouse',	4,	19.99),
+('Teclado',	4,	25.99),
+('Placa de Video - Nvidia 9800 GTX - 256MB/256 bits',	5,	599.99)
+
+INSERT INTO venda
+VALUES
+(1,	1,	'25186533710',	1,	449.99,	 '2009-09-03'),
+(1,	4,	'25186533710',	1,	49.99,	 '2009-09-03'),
+(1,	5,	'25186533710',	1,	349.99,	 '2009-09-03'),
+(2,	6,	'79182639800',	4,	79.96,	 '2009-09-06'),
+(3,	8,	'87627315416',	1,	599.99,  '2009-09-06'),
+(3,	3,	'87627315416',	1,	99.99,	 '2009-09-06'),
+(3,	7,	'87627315416',	1,	25.99,   '2009-09-06'),
+(4,	2,	'34578909290',	2,	1399.98, '2009-09-08')
+
+
+SELECT CONVERT(VARCHAR, data_venda, 103)
+FROM venda
+WHERE venda.codigo = 4
+
+ALTER TABLE fornecedor 
+ADD telefone CHAR(13)
+
+UPDATE fornecedor 
+SET telefone = '72165371'
+WHERE id = 1
+
+UPDATE fornecedor 
+SET telefone = '87153738'
+WHERE id = 2
+
+UPDATE fornecedor 
+SET telefone = '36546289'
+WHERE id = 4
+
+SELECT nome, logr_end + ' ' + CAST(num_end AS VARCHAR) + ' - ' + compl_end + ' - ' + cidade_end AS endereco, telefone
+FROM fornecedor
+ORDER BY nome 
+
+SELECT c.nome, p.descricao, v.quantidade, v.valor_total 
+FROM cliente c, venda v, produto p 
+WHERE c.cpf =  v.cliente 
+	AND v.produto = p.codigo 
+	AND c.nome LIKE 'Julio%'
+
+SELECT c.nome, CONVERT(VARCHAR, v.data_venda, 103) AS data_venda, v.valor_total 
+FROM venda v, cliente c
+WHERE v.cliente = c.cpf 
+	AND c.nome LIKE 'Paulo%'
+	
+SELECT *
+FROM produto
+ORDER BY descricao DESC, preco DESC
