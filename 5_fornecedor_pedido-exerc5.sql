36ee7ebdbb19fb24e0904b1ff8d141c6a37d0bc5
Matheus França <99504777+Matheus-Franca-x@users.noreply.github.com>
2023-11-22 01:42:43 -0300

    Update 5_fornecedor_pedido-exerc5.sql

--- 5_fornecedor_pedido-exerc5.sql
+++ 5_fornecedor_pedido-exerc5.sql
@@ -83,47 +83,57 @@ VALUES
 (99002, 33602,	4,	3,	'2012-06-09'),
 (99003, 33605,	5,	1,	'2012-06-15')
 
+--1) Alterar para reduzir em 10% o valor das barras de chocolate.
 UPDATE produto 
 SET valor_unitario = valor_unitario * 0.9
 WHERE descricão LIKE 'Barra'
 
+--2) Alterar a quantidade em estoque do faqueiro para 10 peças.
 UPDATE produto 
 SET quantidade_estoque = 10
 WHERE nome LIKE 'Faqueiro'
 
+--3) A quantidade, valor total e valor total com desconto (25%) dos itens comprados par Maria Clara.
 SELECT c.nome, p.quantidade * pr.valor_unitario AS valor_total, (p.quantidade * pr.valor_unitario) * 0.75 AS valor_desconto
 FROM cliente c, pedido p, produto pr
 WHERE c.codigo = p.codigo_cliente 
 	AND p.codigo_produto = pr.codigo 
 	AND c.nome LIKE 'Maria%'
-	
+
+--4) Verificar quais brinquedos não tem itens em estoque.
 SELECT *
 FROM produto p, fornecedor f
 WHERE f.codigo  = p.codigo_fornecedor 
 	AND f.atividade LIKE 'Brinquedo'
 	AND p.quantidade_estoque = 0
-	
+
+--5) Quantos clientes tem mais de 40 anos.
 SELECT nome, DATEDIFF(YEAR, data_nasc, GETDATE()) AS idade
 FROM cliente c 
 WHERE DATEDIFF(YEAR, data_nasc, GETDATE()) > 40 
 
+--6) Nome e telefone dos fornecedores de Brinquedos e Chocolate.
 SELECT nome, telefone 
 FROM fornecedor f 
 WHERE atividade LIKE 'Chocolate' OR atividade LIKE 'Brinquedo%'
 
+--7) Nome e desconto de 25% no preço dos produtos que custam menos de R$50,00.
 SELECT nome, valor_unitario * 0.75 AS preco_desconto
 FROM produto p
 WHERE valor_unitario < 50
 
+--8) Nome e aumento de 10% no preço dos produtos que custam mais de R$100,00.
 SELECT nome, valor_unitario * 1.1 AS preco_desconto
 FROM produto p
 WHERE valor_unitario > 100
 
+--9) Desconto de 15% no valor total de cada produto da venda 99001.
 SELECT p.codigo, (p.quantidade * pr.valor_unitario) * 0.85 AS preco_desconto
 FROM pedido p, produto pr
 WHERE p.codigo_produto = pr.codigo 
 	AND p.codigo = 99001
-	
+
+--10) Código do pedido, nome do cliente e idade atual do cliente.
 SELECT p.codigo, c.nome, DATEDIFF(YEAR, c.data_nasc, GETDATE()) AS idade 
 FROM pedido p, cliente c 
 WHERE p.codigo_cliente = c.codigo 
